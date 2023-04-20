0d54a537ca53f68f5850a0b6d14e8fb89292f707
Emo <ipademofdz@gmail.com>
2023-04-20 17:14:36 +0200

    añadida base de datos

--- websocketserver.py
+++ websocketserver.py
@@ -1,12 +1,30 @@
+# pyinstaller videoenplay_une_csv.py --onefile --icon="C:\Users\emo\python\images\proxy.ico"
 import websockets
 import asyncio
 import time
+import sqlite3
+import json
 
 async def websocket_request_handler(websocket, path):
     client_data = await websocket.recv()
     print(time.ctime() + '\n' + client_data + '\n\r')
     response_data = "Datos recibidos por el cliente:\n" + client_data
     await websocket.send(response_data)
+    save_to_database(client_data)
+
+def save_to_database(client_data):
+    conn = sqlite3.connect('datos.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS datos
+                 (identificador INTEGER PRIMARY KEY AUTOINCREMENT,
+                 id TEXT,
+                  time TEXT,
+                  text TEXT)''')
+    
+    data = json.loads(client_data)
+    c.execute('''INSERT INTO datos (id, time, text) VALUES (?, ?, ?)''', (data['id'], data['time'], data['text']))
+    conn.commit()
+    conn.close()
 
 async def start_websocket_server(host, port_number):
     async with websockets.serve(websocket_request_handler, host, port_number):
